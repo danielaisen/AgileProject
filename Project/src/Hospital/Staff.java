64fb008f99630f408e75db7c8b336c6945da9d89
Daniel Max Ajsen <s171206@win.dtu.dk>
2019-03-27 11:33:37 +0100

    Staff starting

---
+++ Project/src/Hospital/Staff.java
@@ -0,0 +1,18 @@
+package Hospital;
+
+
+
+public class Staff extends HospitalUser {
+	
+	private String role;
+//	private Departmnet department;
+	
+	public String getRole() {
+		return role;
+	}
+	
+	public void setRole(String roleName) {
+		this.role = roleName;
+	}
+
+}
