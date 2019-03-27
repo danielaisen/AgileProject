fda5818a4d70068fbe3b3274bec1a048dc3b9b5d
erick <erick@DESKTOP-BP6KAJV.eduroam.wireless.dtu.dk>
2019-03-27 11:36:02 +0100

    test 2

--- Project/src/Hospital/HospitalUser.java
+++ Project/src/Hospital/HospitalUser.java
@@ -10,9 +10,40 @@ public abstract class HospitalUser {
 	private Date birthday;
 	private String gender;
 	
-	public  int getSerialnum() { return serialnum; }
-	public static void main(String args[]) {
-		System.out.println("dooo");
+	public int getSerialnum() { return serialnum; }
+	
+	public String getEmail() { return email; }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
 	}
 }
 
