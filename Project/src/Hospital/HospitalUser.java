ff7838902ae1ab803aeaca19be7df3531f0ce9e1
erick <erick@DESKTOP-BP6KAJV.eduroam.wireless.dtu.dk>
2019-03-27 11:35:13 +0100

    First draft of HospitalUser. Test.

--- Project/src/Hospital/HospitalUser.java
+++ Project/src/Hospital/HospitalUser.java
@@ -1,5 +1,18 @@
 package Hospital;
 
-public class HospitalUser {
+import java.util.Date;
 
+public abstract class HospitalUser {
+	private int serialnum;
+	private String email;
+	private String name;
+	private String surname;
+	private Date birthday;
+	private String gender;
+	
+	public  int getSerialnum() { return serialnum; }
+	public static void main(String args[]) {
+		System.out.println("dooo");
+	}
 }
+
