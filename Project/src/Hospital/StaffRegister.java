f354c88f7e39baf683a82f45386287480ac36e4f
Alexander Christensen <alexfc@live.dk>
2019-03-28 16:41:43 +0100

    Added new modifications

--- Project/src/Hospital/StaffRegister.java
+++ Project/src/Hospital/StaffRegister.java
@@ -1,5 +1,87 @@
 package Hospital;
 
-public class StaffRegister extends Register<Staff> {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class StaffRegister extends Register<Staff> {
+	
+	/**
+	 * Adds a staff member to the register
+	 * @param serialnum
+	 * @param email
+	 * @param name
+	 * @param surname
+	 * @param birthday
+	 * @param gender
+	 * @param role
+	 */
+	public void add(int serialnum, String email, String name, String surname, Date birthday, String gender, String role) {
+		users.add(new Staff(serialnum, email, name, surname, birthday, gender, role));
+		serialnum++;
+	}
+	
+	/**
+	 * Searches for a Staff with a matching Role
+	 * @param role
+	 * @return
+	 */
+	public String[] searchRole(String role) {
+		ArrayList<Staff> matches = findRole(role);
+		String[] m = new String[matches.size()];
+		for (int i = 0; i < matches.size(); i++) {
+			m[i] = matches.get(i).toString();
+		}
+		return m;
+	}
+	
+	/**
+	 * Searches for a Staff with a matching Role
+	 * @param role
+	 * @return
+	 */
+	private ArrayList<Staff> findRole(String role) {
+		ArrayList<Staff> matches = new ArrayList<Staff>();
+		for (Staff p : users) {
+			if (p.getRole() == role) {
+				matches.add(p);
+			}
+		}
+		return matches;
+	}
+	
+	/**
+	 * Searches for a Staff with a matching Gender
+	 * @param role
+	 * @return
+	 */
+	public String[] searchGender(String Gender) {
+		ArrayList<Staff> matches = findGender(Gender);
+		String[] m = new String[matches.size()];
+		for (int i = 0; i < matches.size(); i++) {
+			m[i] = matches.get(i).toString();
+		}
+		return m;
+	}
+	
+	/**
+	 * Searches for a Staff with a matching Gender
+	 * @param role
+	 * @return
+	 */
+	private ArrayList<Staff> findGender(String Gender) {
+		ArrayList<Staff> matches = new ArrayList<Staff>();
+		for (Staff p : users) {
+			if (p.getGender() == Gender) {
+				matches.add(p);
+			}
+		}
+		return matches;
+	}
+	
+	public static void main(String[] args) {
+		StaffRegister test = new StaffRegister();
+		test.add(serialnum, "alexfc@live.dk", "Alexander", "Christensen", new Date(), "male" ,"Doctor");
+		test.add(serialnum, "ehhh@live.dk", "Alexander", "Christensen", new Date(), "x" ,"Nurse");
+		System.out.println(test);
+	}
 }
