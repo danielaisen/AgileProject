c23ac867a3ec3a1e2d6b5e875e871e377a4a1b6f
A <benjy@benjy.dk>
2019-03-29 15:29:30 +0100

    added documentation to Department Register

--- Project/src/Hospital/DepartmentRegister.java
+++ Project/src/Hospital/DepartmentRegister.java
@@ -7,16 +7,40 @@ public class DepartmentRegister extends Department{
 	public DepartmentRegister() {
 		
 	}
-	
-	private Map<String, Department> departments;
-	departments = new Map<String, Department>();
-	
+	//departments is a map mapping string department names to objects of department
+	private Map<String, Department> departments = new Map<String, Department>();
+	/**
+	 * creates a new department and adds it to a map of departments (map: department name -> object of department with that name)
+	 * @param departmentName name you want to give to department 
+	 * @param beds number of beds you want to add to newly created department
+	 */
 	public void createDepartment(String departmentName, int beds) {
 		departments.put(departmentName, new Department(departmentName, beds));
 	}
+	/**
+	 * creates a new department and adds it to a map of departments (map: department name -> object of department with that name)
+	 * @param departmentName name you want to give to department - beds is set to 0
+	 */
+	public void createDepartment(String departmentName) {
+		departments.put(departmentName, new Department(departmentName));
+	}
 	
+	/**
+	 * returns an object of type Department whose name matches the input
+	 * @param departmentName name of department you want to find
+	 * @return
+	 */
 	public Department findDepartment(String departmentName) {
 		return departments.get(departmentName);
 	}
+	/**
+	 * Removes a department from the map of departments, by department name
+	 * @param departmentName name of department you want to remove
+	 */
+	public void deleteDepartment(String departmentName) {
+		departments.remove(departmentName);
+	}
+	
+	
 
 }
