abf04b8e3cd33e67e4756fe540e7617ab225ab69
erick <erickastljensen@gmail.com>
2019-04-03 13:08:00 +0200

    phone -> phoneNumber

--- Project/src/Hospital/PatientRegister.java
+++ Project/src/Hospital/PatientRegister.java
@@ -59,7 +59,7 @@ public class PatientRegister extends Register<Patient> {
 	}
 	
 	/**
-	 * Searches for patients with matching phone phoneNumbers
+	 * Searches for patients with matching phone numbers
 	 * @param phoneNumber
 	 * @return ArrayList of matching patients
 	 */
